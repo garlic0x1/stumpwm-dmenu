54ad0ea348e0999347777caffa44788046b5e378
garlic0x1 <garlic1@protonmail.com>
2024-08-11 08:45:28 -0500

    adsf

--- package.lisp
+++ package.lisp
@@ -1,13 +1,15 @@
 ;;;; package.lisp
 
-(defpackage #:dmenu
+(uiop:define-package #:dmenu
   (:use #:cl #:alexandria)
-  (:export #:*dmenu-position*
+  (:export #:dmenu
+           #:*dmenu-position*
            #:*dmenu-fast-p*
-	   #:*dmenu-case-sensitive-p*
-	   #:*dmenu-font*
-	   #:*dmenu-background-color*
-	   #:*dmenu-foreground-color*
-	   #:*selected-background-color*
-	   #:*dmenu-max-vertical-lines*))
-
+           #:*dmenu-case-sensitive-p*
+           #:*dmenu-font*
+           #:*dmenu-background-color*
+           #:*dmenu-foreground-color*
+           #:*dmenu-selected-background-color*
+           #:*dmenu-selected-foreground-color*
+           #:*selected-background-color*
+           #:*dmenu-max-vertical-lines*))
